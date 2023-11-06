0d1226f15f4012413a616353ce5f2dbe25f48583
Jordan Zucker <jordan.zucker@gmail.com>
2023-11-05 23:33:09 -0800

    One more pass to clean up a bit more

--- qnap_exporter/app/metrics.py
+++ qnap_exporter/app/metrics.py
@@ -14,14 +14,6 @@ class Labels(Enum):
 
 
 class Metrics(object):
-    SEND_MAGIC_PACKET_TIME = Summary(
-        'qnap_exporter_wal_send_magic_pack_time',
-        'Time spent to send WAL magic packet')
-
-    SEND_MAGIC_PACKET_EXCEPTIONS = Counter(
-        'qnap_exporter_wal_send_magic_pack_exceptions',
-        'Exceptions while attempting to send WAL magic packet')
-
     DEBUG_ROUTE_TIME = Summary(
         'qnap_exporter_debug_route_time',
         'Time spent to handle debug route request')

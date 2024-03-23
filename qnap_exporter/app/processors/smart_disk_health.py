e1989c0bd32ecdd9d1c537ef1452da34802f3af3
Jordan Zucker <jordan.zucker@gmail.com>
2024-03-23 12:07:08 -0700

    Simplifying functions

--- qnap_exporter/app/processors/smart_disk_health.py
+++ qnap_exporter/app/processors/smart_disk_health.py
@@ -36,13 +36,12 @@ class SmartDiskUnitsProcessorException(SmartDiskHealthProcessorException):
 
 
 class SmartDiskHealthProcessor(BaseProcessor):
-    def _process_capacity(self, disk_id, disk_stats):
+    def _process_capacity(self, disk_stats):
         capacity = disk_stats.get(SmartDiskDictKeys.CAPACITY)
         capacity_pieces = capacity.split(' ')
         size = capacity_pieces[0]
         units = capacity_pieces[1]
-        c_m = (f'for disk_id: {disk_id} got '
-               f'size: {size} for units: {units}')
+        c_m = (f'size: {size} for units: {units}')
         log.debug(c_m)
         return size, units
 
@@ -86,9 +85,9 @@ class SmartDiskHealthProcessor(BaseProcessor):
             disk_type=disk_type,
             disk_health=health,
         ).set(temp_f)
-        capacity, units = self._process_capacity(
-            smart_disk_id,
-            smart_disk_stats)
+        c_m = f'smart_disk_id: {smart_disk_id} about to process capacity'
+        log.debug(c_m)
+        capacity, units = self._process_capacity(smart_disk_stats)
         Metrics.SMART_DISK_HEALTH_CAPACITY_VALUE.labels(
             nas_name=self.nas_name,
             disk_id=smart_disk_id,
